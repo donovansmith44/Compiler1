469e979e3378a60b88ccd34ef416300a1ca1a0fb
donovansmith44 <donovan.smith44@gmail.com>
2022-11-24 16:30:17 -0500

    commit header file

---
+++ VMWriter.h
@@ -0,0 +1,19 @@
+#ifndef VMWRITER_H
+#define VMWRITER_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <vector>
+#include "JackTokenizer.h"
+#include "CompilationEngine.h"
+
+using namespace std;
+
+class VMWriter
+{
+
+};
+#endif
